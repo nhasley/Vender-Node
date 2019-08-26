1c14947e98067d8fc562c0b629f715079a879cc3
nhasley <novahasley@gmail.com>
2019-08-26 12:45:51 -0500

    added reviews folders for routes/controllers and added server router

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('./config/database')
 
 var indexRouter = require('./routes/index');
 var productsRouter = require('./routes/products');
+var reviewsRouter = require('./routes/reviews');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/products', productsRouter);
+app.use('/', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
