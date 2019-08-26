0bd533c619d6aa0742b5da8aff88e95e0daf1d99
nhasley <novahasley@gmail.com>
2019-08-26 11:40:45 -0500

    set up databse and models for products baics

---
+++ models/product.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    condition: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    }
+}, {
+    timestamps: true
+});
+
+
+module.exports = mongoose.model('Product', productSchema);
