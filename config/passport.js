8a17295790ee785ffb513a8758612d61d706f6f0
nhasley <novahasley@gmail.com>
2019-08-26 20:44:22 -0500

    google login tweeked and redone. Still need edits on the header

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,42 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
-var Product = require('../models/product')
-
+var User = require('../models/user');
 
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_SECRET,
         callbackURL: process.env.GOOGLE_CALLBACK
     },
-    function (accessToken, refreshToken, profile, cb) {}
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOne({
+            'googleId': profile.id
+        }, function (err, user) {
+            if (err) return cb(err);
+            if (user) {
+                return cb(null, user);
+            } else {
+                var newUser = new User({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleId: profile.id,
+                    photo: profile.photos[0].value
+                });
+                newUser.save(function (err) {
+                    if (err) return cb(err);
+                    return cb(null, newUser);
+                });
+            }
+        });
+    }
 ));
 
-passport.serializeUser(function (product, done) {
-    done(null, product.id);
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
 });
 
+// The id here is from the MongoDB id
 passport.deserializeUser(function (id, done) {
-    Product.findById(id, function (err, product) {
-        done(err, product);
+    User.findById(id, function (err, user) {
+        done(err, user);
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 router.get('/', function (req, res, next) {
-  res.redirect('/products');
+  res.render('index', {
+    title: 'Vender',
+    user: req.user
+  });
+});
+
+// function isLoggedIn(req, res, next) {
+//   if (req.isAuthenticated()) return next();
+//   res.redirect('/auth/google');
+// };
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google', {
+    scope: ['profile', 'email']
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google', {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
