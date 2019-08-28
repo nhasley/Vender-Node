6636dfd03852d992dba1c77af660305e7197246d
nhasley <novahasley@gmail.com>
2019-08-28 09:49:19 -0500

    Added delete button

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,21 @@
 var Product = require('../models/product');
 
 module.exports = {
-    create
+    create,
+    delReview
+
 };
+
+function delReview(req, res, next) {
+    Product.findOne({
+        'review._id': req.params.id
+    }, function (err, product) {
+        product.reviews.id(req.params.id).remove();
+        product.save(function (err) {
+            res.redirect('/products');
+        });
+    });
+}
 
 function create(req, res) {
     Product.findById(req.params.id, function (err, product) {
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var reviewsCtrl = require('../controllers/reviews');
 
-router.post('/products/:id/reviews', reviewsCtrl.create);
+router.post('/products/:id/reviews', isLoggedIn, reviewsCtrl.create);
+router.delete('/products/:id/reviews/:id', reviewsCtrl.delReview);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
